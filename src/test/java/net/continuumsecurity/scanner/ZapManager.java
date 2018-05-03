aa91efd8a63a054a55ffd041bfbf30e49a130f04
admin <admin>
2018-05-03 16:01:28 +0200

    update

--- src/test/java/net/continuumsecurity/scanner/ZapManager.java
+++ src/test/java/net/continuumsecurity/scanner/ZapManager.java
@@ -66,14 +66,17 @@ public class ZapManager {
         }
         
         
-        log.info("Start script");
+        log.info("Start script ********");
         File myScriptFile = new File("my_script.sh");
+        log.info("Start script 1 *********");
         ProcessBuilder pb2 = new ProcessBuilder().inheritIO();
+        log.info("Start script 2 *********");
         pb2.directory(myScriptFile.getParentFile());
+        log.info("Start script 3 *********");
         process = pb2.command(String.valueOf(port)).start();
-        log.info(process.getOutputStream().toString());
+        log.info("OUTPUSTREAM *******" + process.getOutputStream().toString());
         Thread.sleep(10000);
-        log.info("Stop script");
+        log.info("Stop script ********");
         
         
         return port;
