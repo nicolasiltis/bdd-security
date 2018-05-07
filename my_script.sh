38bb5d17d03627cb57cd8c12298e6921f6f91d65
nicolasiltis <nicoiltis@estvideo.fr>
2018-05-07 15:42:02 +0200

    Update my_script.sh

--- my_script.sh
+++ my_script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-rm /usr/local/share/ca-certificates/OWASP_ZAP.crt
-rm /etc/ssl/certs/OWASP_ZAP.pem
-curl http://localhost:$1/OTHER/core/other/rootcert/ > /usr/local/share/ca-certificates/OWASP_ZAP.crt
-update-ca-certificates
+sudo rm /usr/local/share/ca-certificates/OWASP_ZAP.crt
+sudo rm /etc/ssl/certs/OWASP_ZAP.pem
+sudo curl http://localhost:$1/OTHER/core/other/rootcert/ > /usr/local/share/ca-certificates/OWASP_ZAP.crt
+sudo update-ca-certificates
 
 #Initialisation of OWASP CA
 certname="OWASP CA"
