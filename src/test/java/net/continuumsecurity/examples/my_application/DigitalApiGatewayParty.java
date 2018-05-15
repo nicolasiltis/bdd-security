8aa3c458ee2136badaec86f73337bf6d68f58fff
Nicolas Iltis <nicoiltis@estvideo.fr>
2018-05-15 07:45:32 +0000

    update of the navigation class

--- src/test/java/net/continuumsecurity/examples/my_application/DigitalApiGatewayParty.java
+++ src/test/java/net/continuumsecurity/examples/my_application/DigitalApiGatewayParty.java
@@ -60,7 +60,7 @@ public class DigitalApiGatewayParty extends WebApplication implements ILogin,
 	
 	public void bookingExternal(){
 	    log.info("Beginning of external booking");
-		driver.get(Config.getInstance().getBaseUrl() + "booking?date=2018-05-11T10:00:00.000Z&storeId=780073");
+		driver.get(Config.getInstance().getBaseUrl() + "booking?date=2018-07-13T10:00:00.000Z&storeId=780073");
 		log.info(driver.getPageSource());
 		findAndWaitForElement(By.id("mat-input-10"));
 		log.info(" mat-input-10 found");
